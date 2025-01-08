24a6a2f73b1b76f1184cd1b6bffdf5d12a48c609
Yuvrajshankar <yuvrajpandar2004@gmail.com>
2025-01-08 14:30:50 +0530

    completed.

---
+++ README.md
@@ -0,0 +1,20 @@
+# Chatly
+
+Chatly is a Full Stack Chatting App.
+Uses Socket.io for real time communication and stores user details in encrypted format in Mongo DB Database.
+
+## Tech Stack
+
+**Client:** Flutter
+
+**Server:** Node JS, Express JS
+
+**Real-time Chatting:** Socket.Io
+
+**Database:** Mongo DB
+
+## Demo
+
+This is mobile responsive App but you can check it functionality on web.
+
+[https://radiant-cuchufli-f0c121.netlify.app/](https://radiant-cuchufli-f0c121.netlify.app)
--- lib/screens/chat.dart
+++ lib/screens/chat.dart
@@ -1,7 +1,9 @@
-import 'package:chatly_flutter/components/chat_messages.dart';
+import 'package:chatly_flutter/provider/user_provider.dart';
 import 'package:chatly_flutter/services/message_services.dart';
-// import 'package:chatly_flutter/services/socket_service.dart';
+import 'package:chatly_flutter/utils/constants.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:socket_io_client/socket_io_client.dart' as IO;
 
 class Chat extends StatefulWidget {
   final String name;
@@ -21,10 +23,104 @@ class Chat extends StatefulWidget {
 
 class _ChatState extends State<Chat> {
   final TextEditingController _messageController = TextEditingController();
+  bool isMessagesLoading = false;
   bool isLoading = false;
   final MessageServices messageServices = MessageServices();
+  final ScrollController _scrollController = ScrollController();
+  List<Map<String, dynamic>> messages = [];
+  IO.Socket? socket;
 
+  @override
+  void initState() {
+    super.initState();
+    _fetchMessages();
+    _initializeSocket();
+
+    // Scroll to the bottom after the widget is built
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      _scrollToBottom();
+    });
+  }
+
+  void _initializeSocket() {
+    socket = IO.io(
+      '${Constants.uri}',
+      IO.OptionBuilder()
+          .setTransports(['websocket'])
+          .disableAutoConnect()
+          .build(),
+    );
+
+    // Connect to the server
+    socket?.connect();
+
+    // Listen for connection
+    socket?.on('connect', (_) {
+      debugPrint('Connected to Socket.IO');
+      socket?.emit('setup', {
+        '_id': Provider.of<UserProvider>(context, listen: false).user.id,
+      });
+    });
+
+    // Listen for new messages
+    socket?.off('message received');
+    socket?.on('message received', (data) {
+      debugPrint('New message received: $data');
+      if (mounted) {
+        setState(() {
+          messages.add(data);
+          // _scrollToBottom();
+        });
+
+        WidgetsBinding.instance.addPostFrameCallback((_) {
+          _scrollToBottom();
+        });
+      }
+    });
+
+    // Handle disconnection
+    socket?.on(
+      'disconnect',
+      (_) => debugPrint('Socket.IO disconnected'),
+    );
+  }
+
+  // Function to scroll to the bottom of the ListView
+  void _scrollToBottom() {
+    _scrollController.animateTo(
+      _scrollController.position.maxScrollExtent,
+      duration: const Duration(milliseconds: 100),
+      curve: Curves.easeOut,
+    );
+  }
+
+  // FETCH MESSAGES
+  Future<void> _fetchMessages() async {
+    setState(() => isMessagesLoading = true);
+    try {
+      // debugPrint("FETCH MESSAGES STARTING");
+      final fetchedMessages =
+          await MessageServices().getMessages(widget.receiverId);
+      // debugPrint("fetchedMessages: ${fetchedMessages.toString()}");
+      setState(() {
+        messages = fetchedMessages;
+        isMessagesLoading = false;
+      });
+
+      // Scroll to bottom after loading messages
+      WidgetsBinding.instance.addPostFrameCallback((_) {
+        _scrollToBottom();
+      });
+    } catch (e) {
+      debugPrint('Error fetching messages: $e');
+      setState(() => isMessagesLoading = false);
+    }
+  }
+
+  // SEND MESSAGE
   void sendMessage() async {
+    final user = Provider.of<UserProvider>(context, listen: false).user;
+
     setState(() {
       isLoading = true;
     });
@@ -34,17 +130,23 @@ class _ChatState extends State<Chat> {
       message: _messageController.text,
     );
 
-    // final message = {
-    //   "receiver": widget.receiverId,
-    //   "message": _messageController.text.trim(),
-    // };
+    final newMessage = {
+      'sender': user.id,
+      'receiver': widget.receiverId,
+      'message': _messageController.text
+    };
 
-    // SocketService.sendMessage(message);
+    // Emit the message to the server
+    socket?.emit('new message', newMessage);
 
     setState(() {
+      // messages.add(newMessage);
       isLoading = false;
       _messageController.clear();
-      // _messageController.text = '';
+    });
+
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      _scrollToBottom();
     });
   }
 
@@ -52,10 +154,13 @@ class _ChatState extends State<Chat> {
   void dispose() {
     super.dispose();
     _messageController.dispose();
+    _scrollController.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
+    final user = Provider.of<UserProvider>(context).user;
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.primary,
@@ -81,9 +186,67 @@ class _ChatState extends State<Chat> {
           Expanded(
             child: Container(
               color: Theme.of(context).colorScheme.background,
-              child: ChatMessages(
-                receiverId: widget.receiverId,
-              ),
+              child: isMessagesLoading
+                  ? const Center(
+                      child: CircularProgressIndicator(
+                        color: Colors.blueAccent,
+                      ),
+                    )
+                  : ListView.builder(
+                      controller:
+                          _scrollController, // Assign the controller to the ListView
+                      itemCount: messages.length,
+                      shrinkWrap: true,
+                      padding: const EdgeInsets.only(top: 10, bottom: 10),
+                      itemBuilder: (context, index) {
+                        final message = messages[index];
+
+                        return isMessagesLoading
+                            ? Center(
+                                child: CircularProgressIndicator(
+                                  color: Colors.blueAccent,
+                                ),
+                              )
+                            : Container(
+                                padding: const EdgeInsets.only(
+                                    left: 14, right: 14, top: 10, bottom: 10),
+                                child: Align(
+                                  alignment: (message["receiver"] == user.id
+                                      ? Alignment.topLeft
+                                      : Alignment.topRight),
+                                  child: Container(
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(20),
+                                      color: (message["receiver"] == user.id
+                                          ? Theme.of(context)
+                                              .colorScheme
+                                              .background
+                                          : Theme.of(context)
+                                              .colorScheme
+                                              .primary),
+                                      border: Border.all(
+                                        color: Theme.of(context)
+                                            .colorScheme
+                                            .primaryContainer, // Border color for sender messages
+                                        width: 2.0, // Border width
+                                      ),
+                                    ),
+                                    padding: const EdgeInsets.all(16),
+                                    child: Text(
+                                      message["message"] ??
+                                          '', // Provide a default value
+                                      style: TextStyle(
+                                        fontSize: 15,
+                                        color: Theme.of(context)
+                                            .colorScheme
+                                            .secondary,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              );
+                      },
+                    ),
             ),
           ),
           Container(
